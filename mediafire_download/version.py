5e53c32d0a228d30306957e0264b94ddafbda340
trickerer01 <onlysuffering+1@gmail.com>
2025-12-30 10:40:11 +0700

    Happy New Year!

--- mediafire_download/version.py
+++ mediafire_download/version.py
@@ -9,7 +9,7 @@ Author: trickerer (https://github.com/trickerer, https://github.com/trickerer01)
 APP_NAME = 'mediafire-download'
 APP_VER_MAJOR = '0'
 APP_VER_SUB = '2'
-APP_REVISION = '27'
+APP_REVISION = '28'
 APP_VERSION = f'{APP_VER_MAJOR}.{APP_VER_SUB}.{APP_REVISION}'
 
 #
